9338170a0b1b5999b2bedd35fe216e7a1c002a62
Stephannie M <rumbi.m@hotmail.co.uk>
2024-02-29 15:56:29 +0000

    generic list view

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kyb&b-7*+u2t*fxd$jrk^loplr25=266h*#fsvz9he$n80c%7t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1','http://127.0.0.1:8000/']
 
