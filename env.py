78f9f04579e0c8fa5767d64ac8b66686f57c2457
Stephannie M <rumbi.m@hotmail.co.uk>
2024-02-29 11:03:35 +0000

    secrect key

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://dzhdeqil:Ih7Ibn62kw0JhjWqBAiJEcCLa9bqhkJX@flora.db.elephantsql.com/dzhdeqil")
+os.environ.setdefault("SECRET_KEY", "ACEGbdg2468~#")
